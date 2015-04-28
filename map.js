2514aefb3b3362997a125f6ce7eb61f42daabad3
Steve Leathers <sayhello@steveleathers.com>
2015-04-28 16:40:54 -0700

    added city limits

--- map.js
+++ map.js
@@ -3,7 +3,7 @@ function initialize() {
   var mapOptions = {
     zoom: 10,
     center: myLatlng,
-    MapTypeId: google.maps.MapTypeId.TERRAIN,
+    MapTypeId: google.maps.MapTypeId.ROADMAP,
     scrollwheel: false
   }
 
@@ -13,7 +13,11 @@ function initialize() {
   var surveyPoints = new google.maps.KmlLayer({
     url: 'http://steveleathers.github.io/EllensburgMaps/SurveyResponses.kmz'
   });
+  var outline = new google.maps.KmlLayer({
+    url: 'http://steveleathers.github.io/EllensburgMaps/EllensburgOutline.kmz'
+  });
   surveyPoints.setMap(map);
+  outline.setMap(map);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
